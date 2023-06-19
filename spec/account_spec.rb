53c461afa5d003fa1f7470799899f70a3fe22833
Alice Wood <alicesophiewood@gmail.com>
2023-06-19 18:54:17 +0100

    balance method and deposit method test driven and implemented

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Account do
       expect(account.list_transactions).to eq([])
     end
   end
+
+  context 'it makes a deposit' do
+    it 'makes a deposit with a given date' do
+      account = Account.new
+      account.deposit(1000, '10/01/2023')
+      expect(account.balance).to eq(1000)
+      expect(account.list_transactions).to eq([[1000, '10/01/2023']])
+    end
+  end
 end
