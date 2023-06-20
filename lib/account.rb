584591e8e6811cd909d042a9001f77b22b910605
Alice Wood <alicesophiewood@gmail.com>
2023-06-20 15:53:27 +0100

    statement being printed and has been test driven

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,20 @@
 require 'date'
 
 class Account
-  def initialize 
-    @total = 0
+  def initialize
     @transactions = []
   end
 
   def list_transactions
-    return @transactions
+    @transactions
   end
 
-  def balance
-    @transactions.each do | transaction |
-      @total = @total + transaction[0].to_i
-      end 
-    return @total
+  def total_balance
+    total = 0
+    @transactions.each do |transaction|
+      total += transaction[0].to_i
+    end
+    total
   end
 
   def deposit(amount, date = Date.today)
@@ -24,4 +24,33 @@ class Account
   def withdrawal(amount, date = Date.today)
     @transactions.push([- amount, date])
   end
+
+  def show_statement
+    statement_headings = "date || credit || debit || balance\n"
+
+    statement_headings + statement_lines.join
+  end
+
+  def statement_lines
+    statement_lines = []
+    @transactions.reverse.each_with_index do |transaction, index|
+      line = if (transaction[0]).positive?
+               "#{transaction[1]} || #{transaction[0]} || || #{current_balance(index)}\n"
+             else
+               "#{transaction[1]} || || #{transaction[0].abs} || #{current_balance(index)}\n"
+             end
+      statement_lines.push(line)
+    end
+    statement_lines
+  end
+
+  def current_balance(current_index)
+    total = 0
+    @transactions.reverse.each_with_index do |transaction, index|
+      total += transaction[0].to_i if index >= current_index
+    end
+    total
+  end
 end
+
+
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Account do
   context 'it constructs' do
     it 'constructs with 0 balance and 0 transactions' do
       account = Account.new
-      expect(account.balance).to eq(0)
+      expect(account.total_balance).to eq(0)
       expect(account.list_transactions).to eq([])
     end
   end
@@ -13,14 +13,14 @@ RSpec.describe Account do
     it 'makes a deposit with a given date' do
       account = Account.new
       account.deposit(1000, '10/01/2023')
-      expect(account.balance).to eq(1000)
+      expect(account.total_balance).to eq(1000)
       expect(account.list_transactions).to eq([[1000, '10/01/2023']])
     end
 
     it 'makes a deposit using default date' do
       account = Account.new
       account.deposit(1000)
-      expect(account.balance).to eq(1000)
+      expect(account.total_balance).to eq(1000)
       expect(account.list_transactions).to eq([[1000, Date.today]])
     end
   end
@@ -30,7 +30,7 @@ RSpec.describe Account do
       account = Account.new
       account.deposit(1000, '10/01/2023')
       account.deposit(2000, '13/01/2023')
-      expect(account.balance).to eq(3000)
+      expect(account.total_balance).to eq(3000)
       expect(account.list_transactions).to eq([[1000, '10/01/2023'], [2000, '13/01/2023']])
     end
   end
@@ -39,14 +39,14 @@ RSpec.describe Account do
     it 'makes a withdrawal with a given date' do
       account = Account.new
       account.withdrawal(500, '14/01/2023')
-      expect(account.balance).to eq(-500)
+      expect(account.total_balance).to eq(-500)
       expect(account.list_transactions).to eq([[-500, '14/01/2023']])
     end
 
     it 'makes a withdrawal using the default date' do
       account = Account.new
       account.withdrawal(500)
-      expect(account.balance).to eq(-500)
+      expect(account.total_balance).to eq(-500)
       expect(account.list_transactions).to eq([[-500, Date.today]])
     end
 
@@ -54,7 +54,7 @@ RSpec.describe Account do
       account = Account.new
       account.withdrawal(500)
       account.withdrawal(500, '14/01/2023')
-      expect(account.balance).to eq(-1000)
+      expect(account.total_balance).to eq(-1000)
       expect(account.list_transactions).to eq([[-500, Date.today], [-500, '14/01/2023']])
     end
   end
@@ -66,8 +66,23 @@ RSpec.describe Account do
       account.deposit(2000, '13/01/2023')
       account.withdrawal(500)
       account.withdrawal(500, '14/01/2023')
-      expect(account.balance).to eq(2000)
-      expect(account.list_transactions).to eq([[1000, '10/01/2023'], [2000, '13/01/2023'], [-500, Date.today], [-500, '14/01/2023']])
+      expect(account.total_balance).to eq(2000)
+      expect(account.list_transactions).to eq([[1000, '10/01/2023'], [2000, '13/01/2023'], [-500, Date.today],
+                                               [-500, '14/01/2023']])
+    end
+  end
+
+  context 'showing the statement' do
+    it 'shows the statement with transactions and the formatted headings' do
+      account = Account.new
+      account.deposit(1000, '10/01/2023')
+      account.deposit(2000, '13/01/2023')
+      account.withdrawal(500, '14/01/2023')
+      expect(account.total_balance).to eq(2500)
+      expect(account.list_transactions).to eq([[1000, '10/01/2023'], [2000, '13/01/2023'], [-500, '14/01/2023']])
+      expect(account.show_statement).to eq(
+        "date || credit || debit || balance\n14/01/2023 || || 500 || 2500\n13/01/2023 || 2000 || || 3000\n10/01/2023 || 1000 || || 1000\n"
+      )
     end
   end
 end
