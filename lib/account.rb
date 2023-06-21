6ffa5b502e53c8833f3899a6dd7098dc0d173fca
Alice Wood <alicesophiewood@gmail.com>
2023-06-21 11:33:20 +0100

    account tests updated and mocked to use new statement class

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,10 @@
 require 'date'
 
 class Account
-  def initialize
+  attr_reader :transactions
+
+  def initialize(statement_printer)
+    @statement_printer = statement_printer
     @transactions = []
   end
 
@@ -26,31 +29,6 @@ class Account
   end
 
   def show_statement
-    statement_headings = "date || credit || debit || balance\n"
-
-    statement_headings + statement_lines.join
-  end
-
-  def statement_lines
-    statement_lines = []
-    @transactions.reverse.each_with_index do |transaction, index|
-      line = if (transaction.amount).positive?
-               "#{transaction.date} || #{transaction.amount} || || #{current_balance(index)}\n"
-             else
-               "#{transaction.date} || || #{transaction.amount.abs} || #{current_balance(index)}\n"
-             end
-      statement_lines.push(line)
-    end
-    statement_lines
-  end
-
-  def current_balance(current_index)
-    total = 0
-    @transactions.reverse.each_with_index do |transaction, index|
-      total += transaction.amount.to_i if index >= current_index
-    end
-    total
+    @statement_printer.generate_statement(@transactions)
   end
 end
-
-
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,31 +2,35 @@ require 'account'
 require 'byebug'
 
 RSpec.describe Account do
-  let(:mock_deposit1) {double(:deposit1, amount: 1000, date: '10/01/2023')}
-  let(:mock_deposit2) {double(:deposit2, amount: 2000, date: '13/01/2023')} 
-  let(:mock_withdrawal1) {double(:withdrawal1, amount: -500, date: '14/01/2023')}
-  let(:mock_withdrawal2) {double(:withdrawal2, amount: -1000, date: '15/01/2023')}
+  let(:mock_deposit1) { double(:deposit1, amount: 1000, date: '10/01/2023') }
+  let(:mock_deposit2) { double(:deposit2, amount: 2000, date: '13/01/2023') }
+  let(:mock_withdrawal1) { double(:withdrawal1, amount: -500, date: '14/01/2023') }
+  let(:mock_withdrawal2) { double(:withdrawal2, amount: -1000, date: '15/01/2023') }
+  let(:mock_statement_printer) do
+    double(:statement_printer,
+           generate_statement: "date || credit || debit || balance\n14/01/2023 || || 500 || 2500\n13/01/2023 || 2000 || || 3000\n10/01/2023 || 1000 || || 1000\n")
+  end
 
   context 'it constructs' do
     it 'constructs with 0 balance and 0 transactions' do
-      account = Account.new
+      account = Account.new(mock_statement_printer)
       expect(account.total_balance).to eq(0)
       expect(account.list_transactions).to eq([])
     end
   end
 
-    it 'makes a deposit' do
-      account = Account.new
-      account.deposit(mock_deposit1)     
-      expect(account.total_balance).to eq(1000)
-      expect(account.list_transactions).to eq([mock_deposit1])
-    end
+  it 'makes a deposit' do
+    account = Account.new(mock_statement_printer)
+    account.deposit(mock_deposit1)
+    expect(account.total_balance).to eq(1000)
+    expect(account.list_transactions).to eq([mock_deposit1])
+  end
 
   context 'multiple deposits' do
     it 'returns the list of transactions and a cumulative balance' do
-      account = Account.new
-      account.deposit(mock_deposit1)  
-      account.deposit(mock_deposit2)    
+      account = Account.new(mock_statement_printer)
+      account.deposit(mock_deposit1)
+      account.deposit(mock_deposit2)
       expect(account.total_balance).to eq(3000)
       expect(account.list_transactions).to eq([mock_deposit1, mock_deposit2])
     end
@@ -34,14 +38,14 @@ RSpec.describe Account do
 
   context 'withdrawals' do
     it 'makes a withdrawal with a given date' do
-      account = Account.new
+      account = Account.new(mock_statement_printer)
       account.withdrawal(mock_withdrawal1)
       expect(account.total_balance).to eq(-500)
       expect(account.list_transactions).to eq([mock_withdrawal1])
     end
 
     it 'makes multiple withdrawals and the account balance is cumulative' do
-      account = Account.new
+      account = Account.new(mock_statement_printer)
       account.withdrawal(mock_withdrawal1)
       account.withdrawal(mock_withdrawal2)
       expect(account.total_balance).to eq(-1500)
@@ -51,7 +55,7 @@ RSpec.describe Account do
 
   context 'deposits and withdrawals made' do
     it 'makes deposits and withdrawals and the account balance reflects correctly' do
-      account = Account.new
+      account = Account.new(mock_statement_printer)
       account.deposit(mock_deposit1)
       account.deposit(mock_deposit2)
       account.withdrawal(mock_withdrawal1)
@@ -64,7 +68,7 @@ RSpec.describe Account do
 
   context 'showing the statement' do
     it 'shows the statement with transactions and the formatted headings' do
-      account = Account.new
+      account = Account.new(mock_statement_printer)
       account.deposit(mock_deposit1)
       account.deposit(mock_deposit2)
       account.withdrawal(mock_withdrawal1)
